91d5486e1a12e8788ea11da211ff90f3eccb4e7f
qassurance408-ui <qassurance408@gmail.com>
2026-06-26 11:52:08 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/vite/',
   plugins: [react()]
 })
-
-export default {
-  base: '/vite/',
-}
